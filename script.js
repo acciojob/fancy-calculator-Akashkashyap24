19a44b5392cb708b2be9b97f19db1a17608e8a2a
Akashkashyap24 <144315919+Akashkashyap24@users.noreply.github.com>
2023-12-31 03:01:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+ function sum(value1,value2) {
+ 	let mul=value1*2;
+	 let result=mul+value2;
+	 return result;
+ }
+let value1 = prompt("enter the value1");
+let value2 = prompt("enter the value2");
+alert("The final result is: "+ sum());
+
+
